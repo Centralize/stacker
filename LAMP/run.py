9df6efdaa64f32b4f28b1dfa7372eda61209d703
Mikkel Kaas <mkaas@openlab.dk>
2022-06-09 13:16:43 +0200

    LAMP Stack updated

--- LAMP/run.py
+++ LAMP/run.py
@@ -14,10 +14,13 @@ def checkRoot():
 
 os.system("apt update; apt upgrade -y;")
 os.system("apt install -y net-tools vim mc htop")
+os.system("apt install lsb-release ca-certificates apt-transport-https software-properties-common -y")
+os.system("add-apt-repository ppa:ondrej/php")
+os.system("apt update;")
 
 apachePresent = os.system("service apache2 status")
 if(apachePresent > 0):
-    os.system("apt install -y apache2")
+    os.system("apt install -y apache2 php8.1 php8.1-cli ")
 else:
     print("Apache2 is already installed.")
 
@@ -27,3 +30,8 @@ if(apachePresent > 0):
 else:
     print("MySQL is already installed.")
 
+phpPresent = os._exists("/etc/php")
+if(phpPresent is not True):
+    os.system("apt install -y php8.1-imagick php8.1-imap php8.1-mysql")
+else:
+    print("PHP 8.1 is already installed.")

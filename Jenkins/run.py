ddec78a8e60814f8fd62daa9be5c5b7783e75543
Mikkel Kaas <mkaas@openlab.dk>
2022-06-10 11:24:47 +0200

    bugfix

--- Jenkins/run.py
+++ Jenkins/run.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-from os import path
-import subprocess
-import socket
 
 
-#hostname = socket.gethostname()
-#local_ip = socket.gethostbyname(hostname)
-
-
-jenkinsPresent = os.system("service jenkins status")
-if(jenkinsPresent is False):
-    os.system("wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -")
-    os.system("sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'")
-    os.system("apt update")
-    os.system("apt install -y jenkins")
-else:
-    print("Jenkins is already installed.\n")
-
-jenkinsPresent = os.system("service jenkins status")
-if(jenkinsPresent is True):
-    print('Jenkins is installed.')
+os.system("wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -")
+os.system("sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'")
+os.system("apt update")
+os.system("apt install -y jenkins")
+print('Jenkins is installed.')

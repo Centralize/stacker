1c04028a8cbe6686c8385a65c35895be4d4c8871
Mikkel Kaas <mkaas@openlab.dk>
2022-06-11 15:27:31 +0200

    module and remote added.

---
+++ settings.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# SSH Settings
+ssh_username="root"
+ssh_password="P@zzW0rd"
+ssh_port=22
--- stacker.py
+++ stacker.py
@@ -3,8 +3,22 @@
 import os
 import sys
 import subprocess
+import paramiko
+from settings import *
+
 
 if(sys.argv is not None):
-    stack = sys.argv[1]
+    contype = sys.argv[2]
 
-subprocess.call(f"{stack}/run.py")
+if contype == "module":
+    stack == (sys.argv[3])
+    subprocess.call(f"{stack}/run.py")
+elif contype == "remote":
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(sys.argv[3],ssh_port,ssh_username,ssh_password,timeout=10)
+    stdin,stdout,stderr = ssh.exec_command("pwd")
+    result = stdout.read()
+    print(result)
+    ssh.close()
+exit(0)

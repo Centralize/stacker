037ce9ee72c6e4295316a195336e76eb161643a7
Mikkel Kaas <mkaas@openlab.dk>
2022-06-23 21:13:04 +0200

    MongoDB Added.

--- LAMP/run.py
+++ LAMP/run.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 from os import path
 
+
 def checkRoot():
     try:
         os.rename('/root/.bash_history', '/root/.bash_history')
@@ -17,7 +18,7 @@ os.system("apt install -y net-tools vim mc htop")
 os.system("apt install lsb-release ca-certificates apt-transport-https software-properties-common -y")
 
 ondrejPresent = path.exists("/etc/apt/sources.list.d/ondrej-ubuntu-php-focal.list")
-if(ondrejPresent is False):
+if (ondrejPresent is False):
     os.system("add-apt-repository ppa:ondrej/php")
 else:
     print("PHP 8.1 Repository is already installed.")
@@ -25,20 +26,19 @@ else:
 os.system("apt update;")
 
 apachePresent = os.system("service apache2 status")
-if(apachePresent > 0):
+if (apachePresent > 0):
     os.system("apt install -y apache2 php8.1 php8.1-cli ")
 else:
     print("Apache2 is already installed.")
 
 mysqlPresent = os.system("service mysql status")
-if(apachePresent > 0):
+if (apachePresent > 0):
     os.system("apt install -y mysql-server mysql-client")
 else:
     print("MySQL is already installed.")
 
 phpPresent = path.exists("/etc/php")
-if(phpPresent is not True):
+if (phpPresent is not True):
     os.system("apt install -y php8.1-imagick php8.1-imap php8.1-mysql")
 else:
     print("PHP 8.1 is already installed.")
-

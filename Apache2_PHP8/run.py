69c4f7cef6b44599d31b433774198a38ed75fa79
Mikkel Kaas <mkaas@openlab.dk>
2022-06-24 11:10:48 +0200

    Apache2_PHP8 module added.

---
+++ Apache2_PHP8/run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from os import path
+
+
+def checkRoot():
+    try:
+        os.rename('/root/.bash_history', '/root/.bash_history')
+    except Exception():
+        if (e[0] is errno.EPERM):
+            sys.exit("You need to run as root or sudo. Exiting.")
+
+
+os.system("apt update; apt upgrade -y;")
+os.system("apt install -y net-tools vim mc htop")
+os.system("apt install lsb-release ca-certificates apt-transport-https software-properties-common -y")
+
+ondrejPresent = path.exists("/etc/apt/sources.list.d/ondrej-ubuntu-php-focal.list")
+if (ondrejPresent is False):
+    os.system("add-apt-repository ppa:ondrej/php")
+else:
+    print("PHP 8.1 Repository is already installed.")
+
+os.system("apt update;")
+
+apachePresent = os.system("service apache2 status")
+if (apachePresent > 0):
+    os.system("apt install -y apache2 php8.1 php8.1-cli ")
+else:
+    print("Apache2 is already installed.")
+
+phpPresent = path.exists("/etc/php")
+if (phpPresent is not True):
+    os.system("apt install -y php8.1 php8.1-common php8.1-cli php8.1-imagick php8.1-imap php8.1-mysql")
+else:
+    print("PHP 8.1 is already installed.")
